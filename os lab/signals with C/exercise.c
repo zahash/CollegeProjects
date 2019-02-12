ebd2a654aa61dd46b6706d46361e9ab4666250c4
zahash <zahash.z@gmail.com>
2019-02-12 15:51:00 +0530

    refer this file for assignment

---
+++ os lab/signals with C/exercise.c
@@ -0,0 +1,33 @@
+//write a program to put a process to sleep and then wake it up and then kill it when completed
+#include<stdlib.h>
+#include<stdio.h>
+#include<signal.h>
+#include<sys/types.h>
+void phalt(int signum)
+{
+printf("Child needs to be killed\n");
+}
+void main()
+{
+pid_t cpid;
+pid_t ppid;
+if((cpid=fork())==0)
+{
+cpid=getpid();
+signal(SIGUSR1,phalt);
+raise(SIGUSR1);
+//while(1);
+}
+else
+{
+kill(cpid,SIGTSTP);
+printf("Child %d halts\n",cpid);
+ppid=getppid();
+printf("Parent %d goes to sleep\n",ppid);
+sleep(5);
+printf("Parent %d awake\n",ppid);
+kill(cpid,SIGCONT);
+printf("Child %d continued\n",cpid);
+wait(NULL);
+}
+}
