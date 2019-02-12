055aefe02a26c4b40b0666898e1b4d3a758947a4
zahash <zahash.z@gmail.com>
2019-02-12 18:13:46 +0530

    prettified the file

--- os lab/signals with C/exercise.c
+++ os lab/signals with C/exercise.c
@@ -3,31 +3,31 @@
 #include<stdio.h>
 #include<signal.h>
 #include<sys/types.h>
-void phalt(int signum)
-{
-printf("Child needs to be killed\n");
+
+void phalt(int signum){
+  printf("Child needs to be killed\n");
 }
-void main()
-{
-pid_t cpid;
-pid_t ppid;
-if((cpid=fork())==0)
-{
-cpid=getpid();
-signal(SIGUSR1,phalt);
-raise(SIGUSR1);
-//while(1);
-}
-else
-{
-kill(cpid,SIGTSTP);
-printf("Child %d halts\n",cpid);
-ppid=getppid();
-printf("Parent %d goes to sleep\n",ppid);
-sleep(5);
-printf("Parent %d awake\n",ppid);
-kill(cpid,SIGCONT);
-printf("Child %d continued\n",cpid);
-wait(NULL);
-}
+
+void main(){
+  pid_t cpid;
+  pid_t ppid;
+  
+  if((cpid=fork())==0){
+    cpid=getpid();
+    signal(SIGUSR1,phalt);
+    raise(SIGUSR1);
+    //while(1);
+  }
+  
+  else{
+    kill(cpid,SIGTSTP);
+    printf("Child %d halts\n",cpid);
+    ppid=getppid();
+    printf("Parent %d goes to sleep\n",ppid);
+    sleep(5);
+    printf("Parent %d awake\n",ppid);
+    kill(cpid,SIGCONT);
+    printf("Child %d continued\n",cpid);
+    wait(NULL);
+  }
 }
